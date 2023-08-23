857133675b69297be5aa092c044c5ee68569f883
Mike <mikefabiosauercuevas@gmail.com>
2023-08-22 22:02:42 -0300

    changes to optmize the script and correction of some errors

--- config_react_native.sh
+++ config_react_native.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
 # Install necessary packages
-sudo apt install -y curl
-sudo apt install -y software-properties-common
+sudo apt install -y curl software-properties-common gcc-multilib lib32z1 lib32stdc++6
 
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install Yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor -o /usr/share/keyrings/yarn-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/yarn-archive-keyring.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y --no-install-recommends yarn
+sudo apt install -y yarn
 
 # Install OpenJDK
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt update
 sudo apt install -y openjdk-11-jdk
 
-# Install additional packages
-sudo apt install -y gcc-multilib lib32z1 lib32stdc++6
-
 # Download and install Android Studio
 curl -o android-studio.tar.gz https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-linux.tar.gz
-tar -xzf android-studio.tar.gz -C ~/android-studio
+tar -xzf android-studio.tar.gz
+mv android-studio-2022.3.1.19-linux ~/android-studio-2022.3.1.19-linux
 rm android-studio.tar.gz
 
 # Set environment variables in .bashrc
